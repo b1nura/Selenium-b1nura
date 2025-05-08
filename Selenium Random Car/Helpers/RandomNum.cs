55201adf2608359071f9a06988d6d4cee434ef1b
b1nura <binurayashi@gmail.com>
2025-05-08 15:20:36 +1000

    new Helper class - random number from var m

---
+++ Selenium Random Car/Helpers/RandomNum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Random_Car.Helpers
+{
+    public class RandomNum
+    {
+        public static int RandomNumber(int num)
+        {
+            Random random = new Random();
+            int newRandomNumber = random.Next(0, num);
+            return newRandomNumber;
+
+           
+        }
+    }
+}
